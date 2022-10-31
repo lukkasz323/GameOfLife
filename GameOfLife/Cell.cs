94da97dbe997603db1fde0b417e7a02f26f7b28c
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-10-31 12:57:43 +0100

    namespace refactor

--- GameOfLife/Cell.cs
+++ GameOfLife/Cell.cs
@@ -1,56 +1,55 @@
-﻿namespace GameOfLife
-{
-    public class Cell
-    {
-        public bool IsAlive { get; set; }
-        public bool IsMarked { get; set; }
+﻿namespace GameOfLife;
 
-        public Cell(bool isAlive, bool isMarked)
+public class Cell
+{
+    public bool IsAlive { get; set; }
+    public bool IsMarked { get; set; }
+
+    public Cell(bool isAlive, bool isMarked)
+    {
+        IsAlive = isAlive;
+        IsMarked = isMarked;
+    }
+
+    public bool CanBeToggled(Cell[,] Grid, int y, int x)
+    {
+        var neighbours = new (int, int)[] // (y, x) offsets
+            {
+                (-1, -1),
+                (-1, 0),
+                (-1, 1),
+                (0, -1),
+                (0, 1),
+                (1, -1),
+                (1, 0),
+                (1, 1)
+            };
+        int neighbours_count = 0;
+
+        foreach((int, int) offset in neighbours)
         {
-            IsAlive = isAlive;
-            IsMarked = isMarked;
+            try
+            {
+                Cell neighbour_cell = Grid[y + offset.Item1, x + offset.Item2];
+
+                if (neighbour_cell.IsAlive)
+                {
+                    neighbours_count++;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                continue;
+            } 
         }
 
-        public bool CanBeToggled(Cell[,] Grid, int y, int x)
+        if (IsAlive)
         {
-            var neighbours = new (int, int)[] // (y, x) offsets
-                {
-                    (-1, -1),
-                    (-1, 0),
-                    (-1, 1),
-                    (0, -1),
-                    (0, 1),
-                    (1, -1),
-                    (1, 0),
-                    (1, 1)
-                };
-            int neighbours_count = 0;
-
-            foreach((int, int) offset in neighbours)
-            {
-                try
-                {
-                    Cell neighbour_cell = Grid[y + offset.Item1, x + offset.Item2];
-
-                    if (neighbour_cell.IsAlive)
-                    {
-                        neighbours_count++;
-                    }
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    continue;
-                } 
-            }
-
-            if (IsAlive)
-            {
-                return !(neighbours_count == 2 || neighbours_count == 3);
-            }
-            else
-            {
-                return (neighbours_count == 3);
-            }
+            return !(neighbours_count == 2 || neighbours_count == 3);
+        }
+        else
+        {
+            return (neighbours_count == 3);
         }
     }
 }
--- GameOfLife/Life.cs
+++ GameOfLife/Life.cs
@@ -1,103 +1,102 @@
-﻿namespace GameOfLife
+﻿namespace GameOfLife;
+
+public class Life
 {
-    public class Life
+    private const string Title = "Game of Life (Enclosed) - Generation: ";
+
+    public Cell[,] Grid { get; set; } = new Cell[Console.WindowHeight - 1, Console.WindowWidth / 2];
+    public int Generation { get; set; } = 1;
+
+    public void MarkCells()
     {
-        private const string Title = "Game of Life (Enclosed) - Generation: ";
-
-        public Cell[,] Grid { get; set; } = new Cell[Console.WindowHeight - 1, Console.WindowWidth / 2];
-        public int Generation { get; set; } = 1;
-
-        public void MarkCells()
+        for (int y = 0; y < Grid.GetLength(0); y++)
         {
-            for (int y = 0; y < Grid.GetLength(0); y++)
+            for (int x = 0; x < Grid.GetLength(1); x++)
             {
-                for (int x = 0; x < Grid.GetLength(1); x++)
-                {
-                    ref Cell cell = ref Grid[y, x];
+                ref Cell cell = ref Grid[y, x];
 
-                    cell.IsMarked = cell.CanBeToggled(Grid, y, x);
+                cell.IsMarked = cell.CanBeToggled(Grid, y, x);
+            }
+        }
+    }
+
+    public void ToggleMarkedCells()
+    {
+        for (int y = 0; y < Grid.GetLength(0); y++)
+        {
+            for (int x = 0; x < Grid.GetLength(1); x++)
+            {
+                ref Cell cell = ref Grid[y, x];
+
+                if (cell.IsMarked)
+                {
+                    cell.IsAlive = !cell.IsAlive;
                 }
             }
         }
+    }
 
-        public void ToggleMarkedCells()
+    public void Initialize()
+    {
+        Console.Title = Life.Title + "1";
+
+        // Init all cells
+        for (int y = 0; y < Grid.GetLength(0); y++)
         {
-            for (int y = 0; y < Grid.GetLength(0); y++)
+            for (int x = 0; x < Grid.GetLength(1); x++)
             {
-                for (int x = 0; x < Grid.GetLength(1); x++)
-                {
-                    ref Cell cell = ref Grid[y, x];
-
-                    if (cell.IsMarked)
-                    {
-                        cell.IsAlive = !cell.IsAlive;
-                    }
-                }
+                Grid[y, x] = new Cell(false, false);
             }
         }
 
-        public void Initialize()
-        {
-            Console.Title = Life.Title + "1";
+        // Starting pattern
+        Grid[16, 24].IsAlive = true;
+        Grid[14, 25].IsAlive = true;
+        Grid[15, 25].IsAlive = true;
+        Grid[16, 25].IsAlive = true;
+        Grid[15, 26].IsAlive = true;
+    }
 
-            // Init all cells
-            for (int y = 0; y < Grid.GetLength(0); y++)
+    public void Update()
+    {
+        MarkCells();
+        ToggleMarkedCells();
+
+        Generation++;
+        Console.Title = Title + Generation;
+    }
+
+    public void Draw()
+    {
+        Console.Clear();
+
+        for (int y = 0; y < Grid.GetLength(0); y++)
+        {
+            for (int x = 0; x < Grid.GetLength(1); x++)
             {
-                for (int x = 0; x < Grid.GetLength(1); x++)
+                ref Cell cell = ref Grid[y, x];
+
+                char sign = cell.IsAlive switch
                 {
-                    Grid[y, x] = new Cell(false, false);
-                }
-            }
+                    false => ' ',
+                    true => '*'
+                };
 
-            // Starting pattern
-            Grid[16, 24].IsAlive = true;
-            Grid[14, 25].IsAlive = true;
-            Grid[15, 25].IsAlive = true;
-            Grid[16, 25].IsAlive = true;
-            Grid[15, 26].IsAlive = true;
-        }
-
-        public void Update()
-        {
-            MarkCells();
-            ToggleMarkedCells();
-
-            Generation++;
-            Console.Title = Title + Generation;
-        }
-
-        public void Draw()
-        {
-            Console.Clear();
-
-            for (int y = 0; y < Grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < Grid.GetLength(1); x++)
-                {
-                    ref Cell cell = ref Grid[y, x];
-
-                    char sign = cell.IsAlive switch
-                    {
-                        false => ' ',
-                        true => '*'
-                    };
-
-                    Console.Write($"{sign} ");
-                }
+                Console.Write($"{sign} ");
             }
         }
+    }
 
-        public void Run()
+    public void Run()
+    {
+        Initialize();
+        Draw();
+        Thread.Sleep(1000);
+        while (true)
         {
-            Initialize();
+            Update();
             Draw();
-            Thread.Sleep(1000);
-            while (true)
-            {
-                Update();
-                Draw();
-                Thread.Sleep(200);
-            }
+            Thread.Sleep(200);
         }
     }
 }
